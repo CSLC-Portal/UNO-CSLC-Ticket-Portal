ec6a2b9da4076035021180c825a15ddc473f8ab4
Spice <kf.shared.ptr@gmail.com>
2023-03-09 00:09:37 -0600

    Directly using default_config module rather than string name

--- portal/app/__init__.py
+++ portal/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from . import default_config
 
 # NOTE: DO NOT change the name of 'create_app()', it is used by gunicorn
 def create_app(name : str):
     new_app = Flask(name)
 
     # Default config is always set to ensure app runs correctly
-    new_app.config.from_object('app.default_config')
+    new_app.config.from_object(default_config)
 
     # Override defaults with any configuration settings from the environment
     new_app.config.from_prefixed_env()
