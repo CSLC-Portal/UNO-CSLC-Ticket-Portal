18b2ca98ab9b3f366e38a36b21cead79c0d94784
Clayton Safranek <claytonsafranek@gmail.com>
2023-03-11 01:34:58 +0000

    setup dummy html page and view for create ticket

--- portal/app/views.py
+++ portal/app/views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
     return 'Future site of the CSLC Tutoring Portal!'
+
+@app.route('/create-ticket')
+def create_ticket():
+    return render_template('create-ticket.html')
