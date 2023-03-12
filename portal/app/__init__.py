b7d0885c9eee5209499aaf2cd5ea9a918853f3ef
Mr-Oregano <kf.shared.ptr@gmail.com>
2023-03-12 23:16:44 +0000

    Added MySQL docker container

--- portal/app/__init__.py
+++ portal/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
 
 from . import default_config
+
+from time import sleep
+from sys import stderr
+import sys
+import os
 
 db = SQLAlchemy()
 
@@ -30,6 +34,36 @@ def setup_env(app : Flask):
 def create_db(app : Flask):
     db.init_app(app)
 
-    # Since SQLAlchemy 3.0+ create_all() requires an app context to run
-    with app.app_context():
-        db.create_all()
+    # TODO: Import model scripts here...
+
+    # NOTE: From the MySQL docker image documentation:
+    #
+    #   "If there is no database initialized when the container starts, then a default database will be created.
+    #   While this is the expected behavior, this means that it will not accept incoming connections until such initialization completes.
+    #   This may cause issues when using automation tools, such as docker-compose, which start several containers simultaneously."
+    #   ...
+    #   "If the application you're trying to connect to MySQL does not handle MySQL downtime or waiting for MySQL to start gracefully,
+    #   then putting a connect-retry loop before the service starts might be necessary."
+    #
+
+    killswitch = int(os.getenv('DATABASE_RECONNECT_ATTEMPTS', 15))
+    cooldown = float(os.getenv('DATABASE_RECONNECT_COOLDOWN', 3))
+
+    while killswitch >= 0:
+        try:
+            # Since SQLAlchemy 3.0+ create_all() requires an app context to run
+            with app.app_context():
+                db.create_all()
+
+            # We succeeded! Break out of this loop
+            print('Successfully connected to database server!')
+            return
+
+        except Exception:
+            print(f'Failed to create database tables, will attempt to reconnect in {cooldown:.2f} seconds...', file=stderr)
+            killswitch -= 1
+            sleep(cooldown)
+            continue
+
+    print(f'Could not connect to or find database server, ensure it is running!', file=stderr)
+    sys.exit(-1)
--- portal/app/default_config.py
+++ portal/app/default_config.py
@@ -11,5 +11,10 @@ PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=30)
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 
+# Flask-SQLAlchemy default options
+# Some database management systems break connection after some time of inactivity (e.g. MySQL)
+# Refresh the connection frequently to circumvent this.
+SQLALCHEMY_ENGINE_OPTIONS = { "pool_recycle": 3600 }
+
 # Default data base is sqlite, this is overriden in production
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
