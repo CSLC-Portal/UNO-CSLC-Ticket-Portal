3d85d63b36f415e18cbc4d3cf9264ced81f2c6a4
Clayton Safranek <claytonsafranek@gmail.com>
2023-03-12 14:56:49 -0500

    Got form submit posting information back to page

--- portal/app/views.py
+++ portal/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def index():
@@ -8,3 +8,17 @@ def index():
 @app.route('/create-ticket')
 def create_ticket():
     return render_template('create-ticket.html')
+
+@app.route('/open-tickets', methods=["POST"])
+def open_tickets():
+    email = request.form.get("emailAdressField")
+    firstName = request.form.get("firstNameField")
+    lastName = request.form.get("lastNameField")
+    course = request.form.get("courseField")
+    section = request.form.get("sectionField")
+    assignmentName = request.form.get("assignmentNameField")
+    specificQuestion = request.form.get("specificQuestionField")
+    problemType = request.form.get("problemTypeField")
+    print("Following ticket information has been created:\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n" % (lastName, firstName, email, course, section, assignmentName, specificQuestion, problemType))
+    return render_template('open-tickets.html', email=email, firstName=firstName, lastName=lastName, course=course,
+                           section=section, assignmentName=assignmentName, specificQuestion=specificQuestion, problemType=problemType)
