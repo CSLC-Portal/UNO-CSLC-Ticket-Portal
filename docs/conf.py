e45f707a91ab014e9448e7d896cd43de239edb0e
Clayton Safranek <claytonsafranek@gmail.com>
2023-04-22 19:46:21 -0500

    config changes for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath("..\\portal\\"))
+sys.path.insert(0, os.path.abspath("../portal"))
 
 project = 'UNO CSLC Portal'
 copyright = '2023, Team GACK'
@@ -26,4 +26,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
