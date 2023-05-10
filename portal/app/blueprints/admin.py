03a1811e048695d90a4dc07f78889da44ace01c3
Spice <kf.shared.ptr@gmail.com>
2023-05-09 21:44:07 -0500

    Some refactorings for report downloads

--- portal/app/blueprints/admin.py
+++ portal/app/blueprints/admin.py
@@ -1,17 +1,14 @@
-from flask import Blueprint, Response, send_from_directory
+from flask import Blueprint
+from flask import Response
 from flask import request
 from flask import flash
 from flask import url_for
 from flask import redirect
 from flask import render_template
-import csv
-import os
-import io
 
 from app.model import ProblemType
 from app.model import Ticket
 from app.extensions import db
-import datetime
 from flask_login import current_user
 
 from app.model import User
@@ -23,7 +20,9 @@ from app.util import str_empty
 from app.util import strip_or_none
 from app.util import permission_required
 
+import csv
 import sys
+import io
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -32,96 +31,69 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 def console():
     return render_template('admin-console.html')
 
-def _strip_or_none(s: str):
-    return s.strip() if s is not None else None
-
-def _str_empty(s: str):
-    return s is not None and not s
-
-@admin.route('/problems', methods=["POST"])
+@admin.route('/problems')
 @permission_required(Permission.Admin)
-def add_problem_type():
-    if request.method == "POST":
-        problemType = _strip_or_none(request.form.get("problemType"))
-        print("PROBLEM TYPE: " + str(problemType))
-
-        # validate the input coming in. store everything in DB the same
-        if _str_empty(problemType):
-            flash('Could not create problem Type, problem type must not be empty!', category='error')
-        else:
-            tmpCourse = ProblemType.query.filter_by(problem_type=problemType).first()
-            if tmpCourse is None:
-                newCourse = ProblemType(problemType)
-                db.session.add(newCourse)
-                db.session.commit()
-                flash('Problem Type created successfully!', category='success')
-                # TODO: return redirect for admin console home?
-            else:
-                flash('Problem Type already exists in database!', category='error')
-                print("Problem Type ALREADY IN DB!")
-
+def view_problem_types():
     problemTypes = ProblemType.query.all()
     return render_template('admin-problem-types.html', problemTypes=problemTypes)
 
-@admin.route('/download', methods=["POST"])
+@admin.route('/problems/add', methods=["POST"])
 @permission_required(Permission.Admin)
-def download_report():
+def add_problem_type():
+    problemType = strip_or_none(request.form.get("problemType"))
 
-    createDate = request.form.get("creationDate")
-    closeDate = request.form.get("closedDate")
+    # validate the input coming in. store everything in DB the same
+    if str_empty(problemType):
+        flash('Could not create problem Type, problem type must not be empty!', category='error')
 
-    courseName = _strip_or_none(request.form.get("course"))
-
-    if courseName:
-        sub = Ticket.query.filter(Ticket.time_created >= createDate, Ticket.time_closed <= closeDate, Ticket.course == courseName).all()
     else:
-        sub = Ticket.query.filter(Ticket.time_created >= createDate, Ticket.time_closed <= closeDate).all()
+        newCourse = ProblemType(problemType)
+        db.session.add(newCourse)
+        db.session.commit()
+        flash('Problem Type created successfully!', category='success')
 
-    global format
-    format = [
-        ['Student Email', 'Student Name', 'Course', 'Section', 'Assignment Name', 'Specific Question', 'Problem Type', 'Time Created', 'Time Claimed',
-         'Status', 'Time Closed', 'Session Duration', 'Mode', 'Tutor Notes', 'Tutor Id', 'Successful Session']
-    ]
-    ticketList = list(sub)
-    lengthTickets = len(ticketList)
+    return redirect(url_for('admin.view_problem_types'))
 
-    for i in range(len(ticketList)):
-        format.append([ticketList[i].student_email, ticketList[i].student_name, ticketList[i].course, ticketList[i].section, ticketList[i].assignment_name,
-                       ticketList[i].specific_question, ticketList[i].problem_type, ticketList[i].time_created, ticketList[i].time_claimed,
-                       ticketList[i].status, ticketList[i].time_closed, ticketList[i].session_duration, ticketList[i].mode, ticketList[i].tutor_notes,
-                       ticketList[i].tutor_id, ticketList[i].successful_session])
-
-    if request.method == "POST":
-        start = request.form.get("creationDate")
-        end = _strip_or_none(request.form.get("closedDate"))
-
-        print(start)
-        print(end)
-
-    return render_template('download-report.html', sub=sub, lengthTickets=lengthTickets)
-
-
-@admin.route('/download_csv', methods=["POST"])
+@admin.route('/reports')
 @permission_required(Permission.Admin)
-def download_csv():
-    file = io.StringIO()
-    writer = csv.writer(file)
-    for line in format:
-        writer.writerow(line)
+def reports_form():
+    return render_template('download-report.html')
 
-    return Response(file.getvalue(), content_type='text/csv', headers={'Content-Disposition': 'attachment;filename=cslc_report.csv'})
+@admin.route('/reports/download', methods=["POST"])
+@permission_required(Permission.Admin)
+def generate_reports():
 
+    # createDate = request.form.get("creationDate")
+    # closeDate = request.form.get("closedDate")
+    # courseName = strip_or_none(request.form.get("course"))
 
-def fix_dde(cell):
-    '''
-    Handles a vulnerability with embedded formulae in csv files
-    '''
-    if cell is not None:
-        cell = str(cell)
-        if cell.startswith(('=', '+', '-', '@')):
-            cell = "' " + cell
-        cell = cell.rstrip()
-    return cell
+    # if courseName:
+    #     tickets = Ticket.query.filter(Ticket.time_created >= createDate, Ticket.time_closed <= closeDate, Ticket.course == courseName).all()
+    # else:
+    #     tickets = Ticket.query.filter(Ticket.time_created >= createDate, Ticket.time_closed <= closeDate).all()
+
+    tickets = Ticket.query.all()
+
+    header = [
+        'Student Email', 'Student Name', 'Course', 'Section', 'Assignment Name', 'Specific Question', 'Problem Type', 'Time Created', 'Time Claimed',
+        'Status', 'Time Closed', 'Session Duration', 'Mode', 'Tutor Notes', 'Tutor Id', 'Successful Session'
+    ]
+
+    with io.StringIO() as out:
+        csv_writer = csv.writer(out)
+        csv_writer.writerow(header)
+
+        for ticket in tickets:
+            row = [ ticket.student_email, ticket.student_name, ticket.course, ticket.section, ticket.assignment_name,
+                    ticket.specific_question, ticket.problem_type, ticket.time_created, ticket.time_claimed, ticket.status,
+                    ticket.time_closed, ticket.mode, ticket.tutor_notes, ticket.tutor_id, ticket.successful_session ]
+
+            map(_bleach, row)
+            csv_writer.writerow(row)
+
+        payload = out.getvalue()
+
+    return Response(payload, content_type='text/csv', headers={'Content-Disposition': 'attachment;filename=cslc_report.csv'})
 
 @admin.route('/tutors')
 @permission_required(Permission.Admin)
@@ -312,6 +284,15 @@ def create_pseudo_super_user(email: str, permission: Permission):
     pseudo_user = User(None, permission, email, None, True, False)
     db.session.add(pseudo_user)
     db.session.commit()
+
+def _bleach(cell):
+    """Will remove characters that might be used to inject formula attacks"""
+    if cell is not None:
+        cell = str(cell)
+        if cell.startswith(('=', '+', '-', '@')):
+            cell = "' " + cell
+        cell = cell.rstrip()
+    return cell
 
 def _attempt_delete_super_user(user: User):
     """
